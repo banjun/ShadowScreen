8d12f92f6404689bff8341c9ede8fa6c941d80c1
banjun <banjun@gmail.com>
2022-01-09 13:55:42 +0900

    Initial Commit

---
+++ ShadowScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShadowScreen
+//
+//  Created by BAN Jun on R 4/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShadowScreen/ShadowScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowScreenApp.swift
+//  ShadowScreen
+//
+//  Created by BAN Jun on R 4/01/09.
+//
+
+import SwiftUI
+
+@main
+struct ShadowScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
