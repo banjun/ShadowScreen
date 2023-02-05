57a9cabe347c44618656bbdc6c56d868fbed9993
banjun <banjun@gmail.com>
2023-02-06 02:05:46 +0900

    adjust color and performance

--- ShadowScreen/CaptureSession.swift
+++ ShadowScreen/CaptureSession.swift
@@ -5,7 +5,7 @@ import CoreImage
 import ScreenCaptureKit
 import Combine
 
-class CaptureSession: NSObject, ObservableObject, SCStreamOutput {
+class CaptureSession: NSObject, ObservableObject, SCStreamOutput, SCStreamDelegate {
     private var stream: SCStream? {
         didSet {
             oldValue?.stopCapture()
@@ -23,15 +23,18 @@ class CaptureSession: NSObject, ObservableObject, SCStreamOutput {
 
     func startRunning(window: SCWindow) {
         let c = SCStreamConfiguration()
-        c.minimumFrameInterval = .init(value: 1001, timescale: 30000)
-        c.queueDepth = 60 // low value or high value cause frame drops
-        c.width = Int(window.frame.width)
-        c.height = Int(window.frame.height)
+        c.minimumFrameInterval = .init(seconds: 1 / 60, preferredTimescale: 10000) // low fps such as 1 / 30 drops frames
+        c.queueDepth = 480 // low value or high value cause frame drops
+//        c.width = Int(window.frame.width)
+//        c.height = Int(window.frame.height)
+//        c.pixelFormat = "420f".utf16.reduce(0) {$0 << 8 + FourCharCode($1)}
+        c.colorSpaceName = CGColorSpace.displayP3 // default value converts to less colors...
+
         c.scalesToFit = true
         if #available(macOS 13.0, *) {
             c.capturesAudio = false
         }
-        stream = SCStream(filter: SCContentFilter(desktopIndependentWindow: window), configuration: c, delegate: nil)
+        stream = SCStream(filter: SCContentFilter(desktopIndependentWindow: window), configuration: c, delegate: self)
         _ = try? stream?.addStreamOutput(self, type: .screen, sampleHandlerQueue: sampleHandlerQueue)
         stream?.startCapture()
     }
@@ -40,11 +43,26 @@ class CaptureSession: NSObject, ObservableObject, SCStreamOutput {
         stream = nil
     }
 
+    private var fpsTimer: Date = .init()
+    private var frames: Int = 0
+
     func stream(_ stream: SCStream, didOutputSampleBuffer sampleBuffer: CMSampleBuffer, of type: SCStreamOutputType) {
         guard type == .screen, sampleBuffer.dataReadiness == .ready else { return }
-        DispatchQueue.main.asyncAfter(deadline: .now() + latency) { [weak self] in
+
+//        frames += 1
+//        if Date().timeIntervalSince(fpsTimer) > 10 {
+//            NSLog("%@", "fps = \(frames / 10)")
+//            frames = 0
+//            fpsTimer = Date()
+//        }
+
+        sampleHandlerQueue.asyncAfter(deadline: .now() + latency) { [weak self] in
             guard let self else { return }
             self.sampleBufferDisplayLayer.enqueue(sampleBuffer)
         }
+    }
+
+    func stream(_ stream: SCStream, didStopWithError error: Error) {
+        NSLog("%@", "\(#function), didStopWithError = \(error)")
     }
 }
